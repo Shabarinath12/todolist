974c9e35a71198efc354c8233587072231a58ee4
Shabrinath Reddy <shabrinathreddy@Shabrinaths-MacBook-Air.local>
2020-01-08 17:15:30 +0530

    commit

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -9,6 +9,12 @@ export class BannerComponent implements OnInit {
 
   name: string ="shabri";
   toggle:boolean=false;
+  // server: boolean=false;
+  switch:boolean=true;
+
+   hide(){
+      this.switch=!this.switch;
+  }
   
   hideme(){
     this.toggle=!this.toggle;
--- src/app/pages/welcomepage/welcomepage.component.ts
+++ src/app/pages/welcomepage/welcomepage.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-welcomepage',
-  templateUrl: './welcomepage.component.html',
-  styleUrls: ['./welcomepage.component.css']
+  selector: "app-welcomepage",
+  templateUrl: "./welcomepage.component.html",
+  styleUrls: ["./welcomepage.component.css"]
 })
 export class WelcomepageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  show: boolean = false;
 
-  ngOnInit() {
-  }
 
+  name: string = 'sai'
+
+  ngOnInit() {}
+
+  profiles: any[] = [
+    {
+      id: "1",
+      img: "img-1",
+      name: "pavan",
+      mobile: 9999999990,
+      email: "pava@gmail.com",
+      pass: "123secrete"
+    },
+    {
+      id: "2",
+      img: "img-1",
+      name: "bharghav",
+      mobile: 9999999990,
+      email: "pava@gmail.com",
+      pass: "123secrete"
+    },
+    {
+      id: "3",
+      img: "img-1",
+      name: "geem",
+      mobile: 9999999990,
+      email: "pava@gmail.com",
+      pass: "123secrete"
+    },
+    {
+      id: "4",
+      img: "img-1",
+      name: "safari",
+      mobile: 9999999990,
+      email: "pava@gmail.com",
+      pass: "123secrete"
+    },
+    {
+      id: "5",
+      img: "img-1",
+      name: "apple",
+      mobile: 9999999990,
+      email: "pava@gmail.com",
+      pass: "123secrete"
+    }
+  ];
 }
